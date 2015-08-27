b4d38a232cacd5865bb7076bb6b7adbff0763a14
Sina Sima <simacna@gmail.com>
2015-08-26 23:10:51 -0400

    working through problem 9

--- problem9.py
+++ problem9.py
@@ -4,4 +4,17 @@
 # For example, 32 + 42 = 9 + 16 = 25 = 52.
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product abc.  ss
+# Find the product abc.
+
+import math
+
+def pythtrip(a,b,c):
+
+	temp = 0
+	if (a+b+c == 1000):
+		if (math.pow(a,2) && math.pow(b,2) == math.pow(c,2)):
+			temp = a*b*c
+
+	return temp
+
+
