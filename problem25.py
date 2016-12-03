6e72801dccb78294d523426d169297536709cbc8
sina sima <simacna@gmail.com>
2016-12-02 22:37:18 -0500

    problem 25 continued

--- problem25.py
+++ problem25.py
@@ -1,6 +1,6 @@
 # #1, 1, 2, 3, 5, 8
 # from datetime import datetime
-
+import time
 # def fib(n):
 #   if n > 1:
 #     return ()
@@ -18,16 +18,38 @@
 #   # print(_)
 
 
-start = datetime.now()
+start = time.clock()
 
 def fib(n):
+  c = 0
   a, b = 1, 1
   for i in range(n-1):
-    print(i)
+    # print(i)
     a, b = b, a + b
-  return a
+    c = b
+    if len(str(c)) == 3:
+      return c
+    print("this is c", )
+  return a #4e-6
+# print(fib(3))
 
-print(datetime().now - start)
+# def fibb(): runs into timeout error
+#   c = 0
+#   a, b = 1, 1
+#   while(len(str(3)) < 2):
+#     print(cß)
+#     a = b
+#     b = b+a
+#     c = b
+#   return c
+# print(fibb())
+
+
+# def fibR(n):
+#   if n>2:
+#     n-1
+#     return fibR()
+# print(time.clock() - start)
 # print(fib(4))
 # print('yo')
 
