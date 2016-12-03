feb6f23b74fc2d1e26ae13524f48e1866f2b8dd6
sina sima <simacna@gmail.com>
2016-12-02 22:18:21 -0500

    m

--- problem25.py
+++ problem25.py
@@ -1,19 +1,52 @@
-#1, 1, 2, 3, 5, 8
+# #1, 1, 2, 3, 5, 8
+# from datetime import datetime
 
-def fib(n):
-  if (n > 0):
-    return(n*fib(n-1))
+# def fib(n):
+#   if n > 1:
+#     return ()
 
 # print(fib(3))
 
-gen = (x for x in range(3))
-print(type(gen))
+# gen = (x for x in range(3))
+# print(type(gen))
 
-# for idx in gen:
-#   print(idx)
+# # for idx in gen:
+# #   print(idx)
 
-li = [x for x in range(3)]
-# for _ in li:
-  # print(_)
+# li = [x for x in range(3)]
+# # for _ in li:
+#   # print(_)
+
+
+start = datetime.now()
+
+def fib(n):
+  a, b = 1, 1
+  for i in range(n-1):
+    print(i)
+    a, b = b, a + b
+  return a
+
+print(datetime().now - start)
+# print(fib(4))
+# print('yo')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
