e6a20b4221eb9264c6cc991f58056a8731f7cc08
sina sima <simacna@gmail.com>
2016-12-12 11:11:01 -0500

    ugh

--- 17.py
+++ 17.py
@@ -1,20 +1,37 @@
-num2words1 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
-            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
-            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
-num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+dict = {0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine'}
 
-def number(Number):
+dict = {0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine'}
 
-    if (Number > 1) or (Number < 19):
-        return (num2words1[Number])
-    elif (Number > 20) or (Number < 99):
-        return (num2words2[Number])
-    else:
-        print("Number Out Of Range")
-        main()
+def number_letters(num):
+    a = 0
+    for idx in range(num):
+        b = [int(i) for i in str(idx)]
+        count = 0
+        for _ in b:
+            count += len(str(_)) #zero, one, two
+        print(count)
 
-def main():
-    num = eval(input("Please enter a number between 0 and 99: "))
-    number(num)
-main()
+        
+
+number_letters(11)
+
+
+
